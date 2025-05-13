0dd19618476ea32c554c22bebdcd59d2a57a2283
slbmax <slobodzian.maxim@gmail.com>
2025-05-13 20:18:34 +0300

    added dummy mailer

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/slbmax/ses-weather-app
 
 go 1.24.0
+
+require github.com/mailjet/mailjet-apiv3-go/v4 v4.0.7
---
+++ pkg/mailer/client.go
@@ -0,0 +1,49 @@
+package mailer
+
+import (
+	"fmt"
+
+	"github.com/mailjet/mailjet-apiv3-go/v4"
+)
+
+type From struct {
+	Email string
+	Name  string
+}
+
+type Client struct {
+	mailjet *mailjet.Client
+	from    From
+}
+
+func NewClient(mailjetKey, mailjetSecret string, from From) *Client {
+	return &Client{
+		mailjet: mailjet.NewMailjetClient(mailjetKey, mailjetSecret),
+		from:    from,
+	}
+}
+
+func (c *Client) Send(to, subject string, bodyHtml string) error {
+	msgInfo := []mailjet.InfoMessagesV31{
+		{
+			From: &mailjet.RecipientV31{
+				Email: c.from.Email,
+				Name:  c.from.Name,
+			},
+			To: &mailjet.RecipientsV31{
+				{
+					Email: to,
+				},
+			},
+			Subject:  subject,
+			HTMLPart: bodyHtml,
+		},
+	}
+
+	msg := &mailjet.MessagesV31{Info: msgInfo}
+	if _, err := c.mailjet.SendMailV31(msg); err != nil {
+		return fmt.Errorf("failed to send email: %w", err)
+	}
+
+	return nil
+}
